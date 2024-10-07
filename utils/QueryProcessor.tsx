3b89fbdb45aa1e6f72acdb943225e4e3ede5ed61
luciafang <yufang@andrew.cmu.edu>
2024-10-07 09:20:53 -0400

    add the andrew id query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,10 +1,15 @@
 export default function QueryProcessor(query: string): string {
+  const lowerCaseQuery = query.toLowerCase();
   if (query.toLowerCase().includes("shakespeare")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
       "English poet, playwright, and actor, widely regarded as the greatest " +
       "writer in the English language and the world's pre-eminent dramatist."
     );
+  }
+
+  if (lowerCaseQuery.includes("what is your andrew id?")) {
+    return "My Andrew ID is yufang."; 
   }
 
   return "";
